75d1b4683668ce7ef65ce42afd9e51fe242a9155
Luke Bell <lukekingsleybell@gmail.com>
2016-06-28 12:14:04 +0200

    Initial commit
    Working android app that can do basic GET and POST requests to server,
    and access accelerometer data.

---
+++ app/src/main/java/com/example/mover/mover/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.mover.mover;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements getRequest.AsyncResponse {
+
+    TextView tv1=null;
+    private double acc;
+    private SensorManager mSensorManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv1 = (TextView) findViewById(R.id.textView);
+        //tv1.setVisibility(View.GONE);
+
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        sensorManager.registerListener(new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+                acc = Math.sqrt(x * x + y * y + z * z);
+
+                tv1.setText("Acceleration: " + acc);
+                //tv1.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+
+        }, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+
+//        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+//        List<Sensor> mList= mSensorManager.getSensorList(Sensor.TYPE_ALL);
+//
+//        for (int i = 1; i < mList.size(); i++) {
+//            //tv1.setVisibility(View.VISIBLE);
+//            tv1.append("\n" + mList.get(i).getName() + "\n" + mList.get(i).getVendor() + "\n" + mList.get(i).getVersion());
+//        }
+
+    }
+
+    /** Called when the user clicks the POST button */
+    public void postMessage(View view) {
+
+        postRequest asyncTask = (postRequest) new postRequest(new postRequest.AsyncResponse(){
+
+            @Override
+            public void processFinish(String output){
+                Context context = getApplicationContext();
+
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, output, duration);
+                toast.show();
+            }
+        }).execute("http://moutonf.co.za:5000/test-post");
+
+    }
+
+    /** Called when the user clicks the GET button */
+    public void getMessage(View view) {
+
+        getRequest asyncTask = (getRequest) new getRequest(new getRequest.AsyncResponse(){
+
+            @Override
+            public void processFinish(String output){
+                Context context = getApplicationContext();
+
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, output, duration);
+                toast.show();
+            }
+        }).execute("http://moutonf.co.za:5000/test-get");
+
+
+    }
+
+
+
+    @Override
+    public void processFinish(String output) {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/mover/mover/postRequest.java
@@ -0,0 +1,87 @@
+package com.example.mover.mover;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Created by LUKE on 2016/06/19.
+ */
+public class postRequest extends AsyncTask<String, String, String> {
+
+    public interface AsyncResponse {
+        void processFinish(String output);
+    }
+
+    public AsyncResponse delegate = null;
+
+    public postRequest(AsyncResponse delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        String urlString = params[0]; // URL to call
+
+        String resultToDisplay = "";
+
+        InputStream in = null;
+        try {
+
+            String urlParameters  = "name=Luke&message=Hello";
+            byte[] postData       = urlParameters.getBytes();
+            int postDataLength = postData.length;
+            URL url            = new URL(urlString);
+
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setDoOutput( true );
+            urlConnection.setInstanceFollowRedirects( false );
+            urlConnection.setRequestMethod( "POST" );
+            urlConnection.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+            urlConnection.setRequestProperty( "charset", "utf-8");
+            urlConnection.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+            urlConnection.setUseCaches( false );
+            urlConnection.getOutputStream().write(postData);
+
+            in = new BufferedInputStream(urlConnection.getInputStream());
+
+
+        } catch (Exception e) {
+
+            System.out.println(e);
+
+            return "error";
+
+        }
+
+        try {
+            resultToDisplay = IOUtils.toString(in, "UTF-8");
+            //to [convert][1] byte stream to a string
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(resultToDisplay);
+        return resultToDisplay;
+    }
+
+
+    @Override
+    protected void onPostExecute(String result) {
+        delegate.processFinish(result);
+    }
+}
